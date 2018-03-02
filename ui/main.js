c6ca4602280b098cf74f5e15fd356021480b0f5d
29abhisheksoni <29abhisheksoni@gmail.com>
2018-03-02 23:12:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,26 +2,27 @@ var button = document.getElementById('counterr');
 
 
 
-button.onclick = function()
+function exec()
 {
-   var request = new XMLHttpRequest();
-   
-   request.onreadystatechange = function(){
-       if(request.readystate === XMLHttpRequest.DONE){
-           if(request.status === 200){
-           
-           var counter = request.responseText;
-           //var span = document.getElementById('countt');
-           //span.innerHTML = counter.toString();
-           document.getElementById('countt').innerHTML = counter.toString();
-           
-           
-           }
-       }
-   };
-   request.open('GET', 'http://u29abhisheksoni.imad.hasura-app.io/counter', true);
-   request.send(null);
-};
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() 
+    {
+        if (request.readyState === XMLHttpRequest.DONE) 
+            {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+            } 
+    }; 
+request.open("GET", "http://u29abhisheksoni.imad.hasura-app.io/counter", true);
+request.send(); 
+console.log('EXECUTED');
+}
 
 
 
