49e9d6e403cf89d75d1081377215c34d75d1aa16
29abhisheksoni <29abhisheksoni@gmail.com>
2018-03-02 12:59:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ button.onclick = function()
    request.send(null);
 };
 
-var nameInput =  document.getElementById('name');
-var name = nameInput.value;
+
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
@@ -53,6 +52,8 @@ submit.onclick = function()
            }
        }
    };
+   var nameInput =  document.getElementById('name');
+var name = nameInput.value;
    request.open('GET', 'http://u29abhisheksoni.imad.hasura-app.io/submit-name?name=' +name, true);
    request.send(null);
    
@@ -69,4 +70,4 @@ submit.onclick = function()
     //var ul = document.getElementById('namelist');
     //ul.innerHTML = list;
     
-}
+};
