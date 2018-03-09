edb9c929b4cf6d229040ba5e480d9361f148564f
29abhisheksoni <29abhisheksoni@gmail.com>
2018-03-09 22:43:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,7 +57,7 @@ submit.onclick = function()
     var password =  document.getElementById('password').value;
     console.log(username);
     console.log(password);
-   request.open('POST', 'https://u29abhisheksoni.imad.hasura-app.io/login', true);
+   request.open('POST', 'http://u29abhisheksoni.imad.hasura-app.io/login',true);
    request.setRequestHeader('Content-Type','application/json');
    request.send(JSON.stringify({'username': username, 'password': password}));
    
